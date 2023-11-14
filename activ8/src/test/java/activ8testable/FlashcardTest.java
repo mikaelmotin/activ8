99b7bdcad4ef8bdbdb5f5c842bafded0be7940ba
Farkas Sutthiburt <farkass@chalmers.se>
2023-11-14 15:27:34 +0100

    comments for FlashcardTest :))

--- activ8/src/test/java/activ8testable/FlashcardTest.java
+++ activ8/src/test/java/activ8testable/FlashcardTest.java
@@ -5,19 +5,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FlashcardTest {
+    //Creates a Flashcard.
     Flashcard testCard = new Flashcard("test", "A card for testing");
+
+    //Tests that the method getTerm() returns the correct term.
     @Test
     void getTerm() {
         String term = testCard.getTerm();
         assertEquals(term, "test");
     }
 
+    //Tests that the method getDefinition() returns the correct definition.
     @Test
     void getDefinition() {
         String definition = testCard.getDefinition();
         assertEquals(definition, "A card for testing");
     }
 
+    //Tests that the method setTerm() edits a Flashcards term correctly.
     @Test
     void setTerm() {
         testCard.setTerm("tested");
@@ -25,6 +30,7 @@ class FlashcardTest {
         assertEquals(newTerm,"tested");
     }
 
+    //Tests that the method setDefinition() edits a Flashcards definition correctly.
     @Test
     void setDefinition() {
         testCard.setDefinition("A tested definition");
