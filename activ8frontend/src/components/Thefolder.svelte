325d228adca0b0d7825a50715a4519c0bdb70092
Ghazal <ghazalg@student.chalmers.se>
2023-12-02 13:54:23 +0100

    new folder comp

---
+++ activ8frontend/src/components/Thefolder.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let imagePath = 'https://icons.iconarchive.com/icons/dtafalonso/yosemite-flat/512/Folder-icon.png';
+    let folderName = 'Untitled Folder';
+    
+  </script>
+  
+  <div class="flex flex-col items-center">
+    <img class="w-30 h-22" src={imagePath} alt="Folder Icon" />
+    <p class="text-xl font-bold">{folderName}</p>
+  </div>
+  
