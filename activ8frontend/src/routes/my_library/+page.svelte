ef09c9ff4d63a04ddf9a33ce31cc557b8073bf34
Ghazal <ghazalg@student.chalmers.se>
2023-12-06 14:24:00 +0100

    design changes in folder and subfolder pages

--- activ8frontend/src/routes/my_library/+page.svelte
+++ activ8frontend/src/routes/my_library/+page.svelte
@@ -39,8 +39,18 @@
 <RouteGuard>
   <div class="flex flex-col w-screen h-screen bg-[#ECF0F5] items-center">
     <nav class="flex w-full mt-4">
-      <img class="h-14 ml-12" src="/Activ8_logo.svg" alt="activ8 logo">
-      <img class="h-14 ml-auto mr-12" src="/user.png" alt="user profile">
+      <div style="width: 100%; height: 100%; position: relative">
+        <div style="width: 77px; height: 77px; left: 20px; top: 0px; position: absolute">
+          <div style="width: 77px; height: 77px; left: 0px; top: 0px; position: absolute; background: #967E76; border-radius: 9999px"></div>
+          <div style="width: 40px; height: 40px; left: 11px; top: 10px; position: absolute; background: white; border-radius: 9999px"></div>
+        </div>
+        <div style="width: 8px; height: 13px; left: 43px; top: 15px; position: absolute; color: #967E76; font-size: 24px; font-family: Impact; font-weight: 400; word-wrap: break-word">8</div>
+      </div>
+          
+      <img class="h-14 ml-auto mr-12" src="/user.png" alt="user profile" />
+      <button class="border border-gray-500 rounded" style="width: 100px; height: 40px; position: absolute; right: 180px; top: 25px; margin-right: 10px;">Edit folders</button>
+      <button class="border border-gray-500 rounded" style="width: 100px; height: 40px; position: absolute; right: 300px; top: 25px;">Add folders</button>
+
     </nav>
     <div class="flex mt-4 mx-12 bg-white w-[80%] h-full justify-center">
       <!-- Folders are created below -->
