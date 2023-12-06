5730d249b6399c5adc848ecf8760d69fb4b0b960
mikaelmotin <motin@chalmers.se>
2023-12-06 22:42:44 +0100

    finished createStudySet

--- activ8frontend/src/components/FlashcardComponent.svelte
+++ activ8frontend/src/components/FlashcardComponent.svelte
@@ -1,11 +1,13 @@
 <script>
+    import { fade } from 'svelte/transition';
+
     export let flashcard;
     export let removeFlashcard;
     export let index;
 </script>
   
 
-<div class="flex flex-col w-full h-48 mb-4 shadow-md rounded-lg">
+<div class="flex flex-col w-full h-48 mb-4 shadow-md rounded-lg" transition:fade="{{ duration: 300 }}" on:outroend={() => removeFlashcard(index)}>
 
  
 
