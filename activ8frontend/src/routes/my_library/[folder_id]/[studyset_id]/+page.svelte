7a6c86c94c173638a31628acee5d6ff32ecf616a
Mikael Motin <motin@chalmers.se>
2023-12-07 18:01:51 +0100

    Delete activ8frontend/src directory

--- activ8frontend/src/routes/my_library/[folder_id]/[studyset_id]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import RouteGuard from "../../../../components/RouteGuard.svelte";
-
-    // Flashcard data:
-    export let data;
-    console.log(data)
-</script>
-
-<p>Show flashcards here</p>
--- activ8frontend/src/stores/authStore.js
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-
-// Create a writable store with an initial value of false (not authenticated)
-export const isAuthenticated = writable(false);
