a12719ed6731f3a111cc7bf218be87617c7fd1f9
Annelie <annelieh@chalmers.se>
2023-12-08 17:17:23 +0100

    Title och description fungerar på Edit

--- activ8frontend/src/routes/my_library/[folder_id]/[studyset_id]/+page.js
+++ activ8frontend/src/routes/my_library/[folder_id]/[studyset_id]/+page.js
@@ -1,33 +1,10 @@
-export const load = async ({ fetch, params }) => {
+export const load = ({ fetch, params }) => {
+    console.log(params);
     const studyset_id = params.studyset_id;
-    const folder_id = params.folder_id;
 
-    const fetchStudySetData = async () => {
+    const fetchStudySet = async (id) => {
         try {
-            const response = await fetch(`http://localhost:8080/api/studysets/${studyset_id}`, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                credentials: "include",
-            });
-
-            if (response.ok) {
-                const studySetData = await response.json();
-                return studySetData;
-            } else {
-                console.error("Request failed with status:", response.status);
-                return null;
-            }
-        } catch (error) {
-            console.error("Network error:", error);
-            return null;
-        }
-    };
-
-    const fetchFlashcards = async () => {
-        try {
-            const response = await fetch(`http://localhost:8080/api/flashcards/all/${studyset_id}`, {
+            const response = await fetch('http://localhost:8080/api/studysets/' + studyset_id, {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
@@ -37,23 +14,46 @@ export const load = async ({ fetch, params }) => {
 
             if (response.ok) {
                 const data = await response.json();
+                console.log("Data:", data);
                 return data;
             } else {
                 console.error("Request failed with status:", response.status);
-                return null;
+                return false;
             }
         } catch (error) {
             console.error("Network error:", error);
-            return null;
+            return false;
         }
-    };
+    }
 
-    const [studySetData, flashcards] = await Promise.all([fetchStudySetData(), fetchFlashcards()]);
+
+
+    const fetchFlashcards = async (id) => {
+        try {
+            const response = await fetch('http://localhost:8080/api/flashcards/all/' + studyset_id, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: "include",
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log("Data:", data);
+                return data;
+            } else {
+                console.error("Request failed with status:", response.status);
+                return false;
+            }
+        } catch (error) {
+            console.error("Network error:", error);
+            return false;
+        }
+    }
 
     return {
-        studySetData: studySetData,
-        flashcards: flashcards,
-        folder_id: folder_id,
-        studyset_id: studyset_id,
+        flashcards: fetchFlashcards(params),
+        studyset: fetchStudySet(params)
     };
-};
+}
