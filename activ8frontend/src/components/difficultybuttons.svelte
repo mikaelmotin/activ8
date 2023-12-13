290ac4b96cfa4feaeb66dc274211b130adfc3dd0
Annelie <annelieh@chalmers.se>
2023-12-13 20:32:27 +0100

    Session frontend typ

--- activ8frontend/src/components/difficultybuttons.svelte
+++ activ8frontend/src/components/difficultybuttons.svelte
@@ -1,69 +1,19 @@
-<!-- DifficultyButtons.svelte -->
-
 <script>
-    // You can leave this script section empty if you don't need any logic for now
-  </script>
-  
-  <div class="main-container">
-    <header class="header">Difficulty Levels:</header>
-    <div class="difficulty-container">
-      <!-- When a difficulty button is clicked, call loadNextFlashcard -->
-      <button class="easy-button">Easy</button>
-      <button class="medium-button">Medium</button>
-      <button class="hard-button">Hard</button>
-    </div>
-  </div>
-  
-  <style>
-  .main-container {
-    position: absolute;
-    top: 52%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px;
-    z-index: 1;
-  }
+  // You can leave this script section empty if you don't need any logic for now
+</script>
 
-  
-    .difficulty-container {
-      display: flex;
-      justify-content: space-between;
-      gap: 20px;
-    }
-  
-    .easy-button,
-    .medium-button,
-    .hard-button {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding: 15px 20px;
-      font-weight: bold;
-      font-size: 15px;
-      text-align: center;
-      border-radius: 10px;
-      box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-      color: #000;
-      width: 131px;
-      height: 34px;
-      flex-shrink: 0;
-      font-family: Inconsolata, -apple-system, Roboto, Helvetica, sans-serif;
-      
-    }
-  
-    .easy-button {
-      background-color: rgba(215, 192, 174, 0.3);
-    }
-  
-    .medium-button {
-      background-color: rgba(215, 192, 174, 0.65);
-    }
-  
-    .hard-button {
-      background-color: #d7c0ae;
-    }
-  </style>
-  
+<div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center p-5 z-10">
+  <header class="text-xl font-bold mb-4">Difficulty Levels:</header>
+  <div class="flex justify-between">
+    <!-- When a difficulty button is clicked, call loadNextFlashcard -->
+    <button class="easy-button px-4 py-3 font-bold text-center border rounded-md box-border focus:outline-none focus:ring focus:border-blue-300">
+      Easy
+    </button>
+    <button class="medium-button px-4 py-3 font-bold text-center border rounded-md box-border focus:outline-none focus:ring focus:border-blue-300">
+      Medium
+    </button>
+    <button class="hard-button px-4 py-3 font-bold text-center border rounded-md box-border focus:outline-none focus:ring focus:border-blue-300">
+      Hard
+    </button>
+  </div>
+</div>
---
+++ activ8frontend/src/routes/my_library/[folder_id]/[studyset_id]/studySession/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Flashcard from '../../../../../components/flashcard.svelte';
+
+  let front = 'Question';
+  let back = 'Answer';
+  let isFlipped = false;
+
+  function flipCard() {
+    isFlipped = !isFlipped;
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      flipCard();
+    }
+  }
+  
+</script>
+
+<main class="bg-gray-100 flex flex-col min-h-screen">
+  <div class="flex flex-col">
+    <div class="flex flex-col bg-gray-100 inset-0 overflow-hidden relative min-h-[1117px] w-full items-end px-8 py-50 shadow-inner">
+      <!-- Include the Flashcard component -->
+      <Flashcard {front} {back} {isFlipped} {flipCard} {handleKeyDown} />
+
+      <!-- Difficulty Level Buttons -->
+      <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center p-5 z-10">
+        <header class="text-xl font-bold mb-4">Difficulty Levels:</header>
+        <div class="flex justify-between">
+          <button class="easy-button px-4 py-3 font-bold text-center border rounded-md box-border focus:outline-none focus:ring focus:border-blue-300">
+            Easy
+          </button>
+          <button class="medium-button px-4 py-3 font-bold text-center border rounded-md box-border focus:outline-none focus:ring focus:border-blue-300">
+            Medium
+          </button>
+          <button class="hard-button px-4 py-3 font-bold text-center border rounded-md box-border focus:outline-none focus:ring focus:border-blue-300">
+            Hard
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
